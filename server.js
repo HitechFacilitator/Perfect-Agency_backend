5fbda9c65e08ec150a7f549e7ab0a7bd16210549
HitechAngel <hitechangel@localhost.localdomain>
2026-03-30 20:50:38 +0100

    Fix deployement issue

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
 	process.env.CLIENT_URL,
 	"http://localhost:5173",
-	"http://127.0.0.1:5173",
+	"https://perfect-agency-frontend.vercel.app/",
 ].filter(Boolean);
 
 app.use(express.json());
